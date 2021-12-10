4a37c769d755f314930a59164a50a096053ec90e
Victor <nemecvictor.kirsh@gmail.com>
2021-12-10 17:25:55 +0300

    init store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,1 +1,27 @@
-import {} from 'vue';
+import {createStore, useStore, Store} from 'vuex';
+import {UserType} from '../types/user';
+
+type MainStateTypes = {
+    user: UserType,
+    isAuthorized: boolean,
+}
+
+export default createStore<MainStateTypes>({
+  state: () => {
+    return {
+      user: {} as UserType,
+      isAuthorized: false,
+    };
+  },
+  mutations: {
+    setUser: (state, value: UserType) => {
+      state.user = value;
+      state.isAuthorized = true;
+    },
+    logout: (state) => {
+      state.user = {} as UserType;
+      state.isAuthorized = false;
+    },
+
+  },
+});
